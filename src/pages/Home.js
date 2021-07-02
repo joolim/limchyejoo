05b4c49cc4053307f593e5a50db38a3f58b7d90d
joolim <joolim625@gmail.com>
2021-07-02 12:52:24 +0800

    amend image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,9 @@ function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage2})` }}>
       <div className="headerContainer">
-        <h1 style="color:red"> Joo's Pizzeria </h1>
-        <p style="color:red">Ho Chiak and Shiok!</p>
-        <p style="color:red"> Dine In and Takeway</p>
-        <br></br>
-        <p style="color:red"> NUS Branch Opening Soon!</p>
+        <h1> Joo's Pizzeria </h1>
+        <p>Ho Chiak and Shiok!</p>
+        <p>Dine In and Takeway</p>
         <Link to="/menu">
           <button> ORDER NOW </button>
         </Link>
