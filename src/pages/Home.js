113390a13884f69db5b9369c3b61ae4833e3eaf8
joolim <joolim625@gmail.com>
2021-07-02 12:28:27 +0800

    updated images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,29 +6,18 @@ import "../styles/Home.css";
 
 function Home() {
   return (
-    (
-      <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
-        <div className="headerContainer">
-          <h1> Joo's Pizzeria </h1>
-          <p> Ho Chiak and Shiok!</p>
-          <p> Dine In and Takeway</p>
-          <Link to="/menu">
-            <button> ORDER NOW </button>
-          </Link>
-        </div>
+    <div className="home" style={{ backgroundImage: `url(${BannerImage2})` }}>
+      <div className="headerContainer">
+        <h1> Joo's Pizzeria </h1>
+        <p>Ho Chiak and Shiok!</p>
+        <p> Dine In and Takeway</p>
+        <br></br>
+        <p> NUS Branch Opening Soon!</p>
+        <Link to="/menu">
+          <button> ORDER NOW </button>
+        </Link>
       </div>
-    ),
-    (
-      <div
-        className="aboutBottom"
-        style={{ backgroundImage: `url(${BannerImage2})` }}
-      >
-        <div className="headerContainer">
-          <h1> NUS Branch </h1>
-          <p> Opening Soon!</p>
-        </div>
-      </div>
-    )
+    </div>
   );
 }
 
