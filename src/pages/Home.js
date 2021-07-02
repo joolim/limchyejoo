b6114937b706952bd1e349a780a60984df07bb0f
joolim <joolim625@gmail.com>
2021-07-02 10:25:18 +0800

    updated brand

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,8 @@ function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
-        <h1> Pedro's Pizzeria </h1>
-        <p> PIZZA TO FIT ANY TASTE</p>
+        <h1> Bikers Pizzeria </h1>
+        <p> Journey round the world. Just remember your Pizzeria</p>
         <Link to="/menu">
           <button> ORDER NOW </button>
         </Link>
