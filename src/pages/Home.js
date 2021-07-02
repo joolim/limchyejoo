8ced4faeb10d16d99426eb1fc6dd70d743318c76
joolim <joolim625@gmail.com>
2021-07-02 11:05:01 +0800

    NUS branch soon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BannerImage from "../assets/pizza.jpeg";
+import BannerImage2 from "../assets/nus01.png";
 import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
-      <div className="headerContainer">
-        <h1> Ah Joo's Pizzeria </h1>
-        <p> Ho Chaik and Shiok!</p>
-        <p> Dine In and Takeway</p>
-        <Link to="/menu">
-          <button> ORDER NOW </button>
-        </Link>
+    (
+      <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+        <div className="headerContainer">
+          <h1> Ah Joo's Pizzeria </h1>
+          <p> Ho Chiak and Shiok!</p>
+          <p> Dine In and Takeway</p>
+          <Link to="/menu">
+            <button> ORDER NOW </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    ),
+    (
+      <div className="home" style={{ backgroundImage: `url(${BannerImage2})` }}>
+        <div className="headerContainer">
+          <h1> NUS Branch </h1>
+          <p> Opening Soon!</p>
+        </div>
+      </div>
+    )
   );
 }
 
