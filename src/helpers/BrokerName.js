fb8fc978d100da3b451a2a2fb255162fa9244a94
joolim <joolim625@gmail.com>
2021-08-06 15:06:59 +0800

    BrokerName amended

---
+++ src/helpers/BrokerName.js
@@ -0,0 +1,39 @@
+import Alpaca from "../assets/alpaca-logo.png";
+import Bitfinex from "../assets/bitfinex-logo.png";
+import Coinbase from "../assets/coinbase-logo.png";
+import FXCM from "../assets/fxcm-logo.png";
+import Interactive from "../assets/interactive-logo.png";
+import Oanda from "../assets/oanda-logo.png";
+
+export const BrokerName = [
+  {
+    name: "Alpaca",
+    image: Alpaca,
+    price: 15000.99,
+  },
+  {
+    name: "Bitfinex",
+    image: Bitfinex,
+    price: 1100.19,
+  },
+  {
+    name: "Coinbase",
+    image: Coinbase,
+    price: 25600.53,
+  },
+  {
+    name: "FXCM",
+    image: FXCM,
+    price: 17004.11,
+  },
+  {
+    name: "Interactive",
+    image: Interactive,
+    price: 4.99,
+  },
+  {
+    name: "Oanda",
+    image: Oanda,
+    price: 1997.99,
+  },
+];
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrokerName } from "../helpers/BrokerName";
 import { MenuList } from "../helpers/MenuList";
 import MenuItem from "../components/MenuItem";
 import "../styles/Menu.css";
