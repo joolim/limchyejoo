54fd652e5ef01fe76fafc56956886664b15b4dae
joolim <joolim625@gmail.com>
2021-08-06 17:01:39 +0800

    added BrokerName fn in components

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -9,5 +9,14 @@ function MenuItem({ image, name, price }) {
     </div>
   );
 }
-
+function BrokerName({ image, name, price }) {
+  return (
+    <div className="menuItem">
+      <div style={{ backgroundImage: `url(${image})` }}> </div>
+      <h3> {name} </h3>
+      <p> ${price} </p>
+    </div>
+  );
+}
+export default BrokerName;
 export default MenuItem;
