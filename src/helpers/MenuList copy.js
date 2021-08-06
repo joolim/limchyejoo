01597ba0e38f3e7221534f03571b14fd71047cc9
joolim <joolim625@gmail.com>
2021-08-06 15:53:21 +0800

    just added alpaca to broker menu

---
+++ src/helpers/MenuList copy.js
@@ -0,0 +1,39 @@
+import Alpaca from "../assets/alpaca-log.png";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Alpaca",
+    image: Alpaca,
+    price: 15000.99,
+  },
+  {
+    name: "Github Pizza",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "Fintech Special Pizza",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Netlify Pizza",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Heroku Pizza",
+    image: Pineapple,
+    price: 4.99,
+  },
+  {
+    name: "Bitcoin Pizza",
+    image: Expensive,
+    price: 1997.99,
+  },
+];
